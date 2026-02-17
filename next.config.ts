0b491c49c9588d49275860c9aa0c0a68bc53a850
shunshuu69 <shunshuu69@gmail.com>
2026-02-17 13:26:38 +0700

    Disable webpack cache to fix Cloudflare Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config, { isServer }) => {
+    // Disable webpack cache for Cloudflare Pages
+    config.cache = false;
+    return config;
+  },
 };
 
 export default nextConfig;
